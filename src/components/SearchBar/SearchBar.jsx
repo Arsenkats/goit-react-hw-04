780c24609a6a83b323efc76905c5025e04235868
Arsenkats <kats45mam@gmail.com>
2024-10-11 12:14:28 +0300

    fix styles

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import css from "./Header.module.css";
+import css from "./SearchBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
